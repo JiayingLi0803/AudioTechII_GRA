641b2aa7a7ada22851425e0755f786470807f86f
Jiaying Li <jiaying.li0803@gmail.com>
2023-03-23 15:58:34 -0400

    correlation

--- autocorrelation.py
+++ autocorrelation.py
@@ -8,145 +8,124 @@ def exp_func(t, a):
     return np.exp(-a * t) * (t >= 0)
 
 # Define the unit step function
-def unit_func(t):
-    return 1.0 * (t >= 0)
+def rect_func(t, a):
+    return np.piecewise(t, [t < 0, (t>=0)*(t<a), t >= a], [0, 1, 0])
 
 # Define the dirace impulse
-def dirac_func(t):
-    return 1.0 * (abs(t-0.006) < 1e-4)
+def tri_func(t, a):
+    return np.piecewise(t, [t < 0, (t>=0)*(t<a), t >= a], [0, lambda t: a-t, 0])
 
-# Define the convolution function
-def convolve(signal, impulse, time):
+# Define correlation function
+def corr(signal, impulse, time):
     dt = time[1] - time[0]
-    result = np.zeros_like(time)
-    for i in range(len(time)):
-        # result[i] = np.sum(signal[:i+1] * impulse[i::-1]) * dt
-        result[i] = np.sum(signal[:i+1] * impulse[i::-1][::-1]) * dt
-    return result
+    result = np.correlate(signal, impulse, "full")
+    print(len(result), result)
+    return result[500:1500] * dt
 
-# Define the impulse response flip signal
-def flip(signal, time, timeshift):
+# Define shift function
+def shift(signal, time, shifttime):
     dt = time[1] - time[0]
-    shift = int((timeshift+2)/dt)
-    result = signal[shift::-1]
-    result = np.pad(result, (0, 1000-len(result)), "constant")
+    pts = int(dt*shifttime)
+    if shifttime >= 0:
+        result = signal[:len(signal)-pts]
+        result = np.pad(result, (1000-len(result), 0), "constant")
+    else:
+        result = signal[pts:]
+        result = np.pad(result, (0, 1000-len(result)), "constant")
     return result
-
-
-
 
 # Generate input and impulse response signals
 # Set up the time axis
-t = np.linspace(-1, 5, 1000)
+t = np.linspace(-5, 5, 1000)
 # Set up the functions to convolve
-x = unit_func(t)
+x = rect_func(t, 1)
 h = exp_func(t, 1)
-hinv = flip(h, t, 0)
-y = convolve(x, h, t)
-y2 = convolve(x, h, t)
-# # x func
-# x_unit = unit_func(t)
-# x_dirc = dirac_func(t)
-# x_exp = exp_func(t, 1)
-# # h func
-# h_unit = unit_func(t)
-# h_dirc = dirac_func(t)
-# h_exp = exp_func(t, 1)
-# # hinv func
-# hinv_unit = flip(h_unit, t, 0)
-# hinv_dirc = flip(h_dirc, t, 0)
-# hinv_dirc = flip(h_exp, t, 0)
-# # y func
-# y_unit_unit = convolve(x_unit, h_unit, t)
-# y_unit_dirc = convolve(x_unit, h_dirc, t)
-# y_unit_exp = convolve(x_unit, h_exp, t)
-# y_dirc_unit = convolve(x_dirc, h_unit, t)
-# y_dirc_dirc = convolve(x_dirc, h_dirc, t)
-# y_dirc_exp = convolve(x_dirc, h_exp, t)
-# y_exp_unit = convolve(x_exp, h_unit, t)
-# y_exp_dirc = convolve(x_exp, h_dirc, t)
-# y_exp_exp = convolve(x_exp, h_exp, t)
-# # y2 func
-# y2_unit_unit = convolve(x_unit, h_unit, t)
-# y2_unit_dirc = convolve(x_unit, h_dirc, t)
-# y2_unit_exp = convolve(x_unit, h_exp, t)
-# y2_dirc_unit = convolve(x_dirc, h_unit, t)
-# y2_dirc_dirc = convolve(x_dirc, h_dirc, t)
-# y2_dirc_exp = convolve(x_dirc, h_exp, t)
-# y2_exp_unit = convolve(x_exp, h_unit, t)
-# y2_exp_dirc = convolve(x_exp, h_dirc, t)
-# y2_exp_exp = convolve(x_exp, h_exp, t)
+hshift = shift(h, t, 0)
+y = corr(x, h, t)
+y2 = corr(x, h, t)
+# x func
+x_rect = rect_func(t, 1)
+x_tri = tri_func(t, 1)
+x_exp = exp_func(t, 1)
+# h func
+h_rect = rect_func(t, 1)
+h_tri = tri_func(t, 1)
+h_exp = exp_func(t, 1)
+# hshift func
+hshift_rect = shift(h_rect, t, 0)
+hshift_tri = shift(h_tri, t, 0)
+hshift_exp = shift(h_exp, t, 0)
+# y func
+y_rect_rect = corr(x_rect, h_rect, t)
+y_rect_tri = corr(x_rect, h_tri, t)
+y_rect_exp = corr(x_rect, h_exp, t)
+y_tri_rect = corr(x_tri, h_rect, t)
+y_tri_tri = corr(x_tri, h_tri, t)
+y_tri_exp = corr(x_tri, h_exp, t)
+y_exp_rect = corr(x_exp, h_rect, t)
+y_exp_tri = corr(x_exp, h_tri, t)
+y_exp_exp = corr(x_exp, h_exp, t)
 
 for i in range(len(y2)):
     if i>=1:
         y2[i] = np.inf
-        # y2_unit_unit[i] = np.inf
-        # y2_unit_dirc[i] = np.inf
-        # y2_unit_exp[i] = np.inf
-        # y2_dirc_unit[i] = np.inf
-        # y2_dirc_dirc[i] = np.inf
-        # y2_dirc_dirc[i] = np.inf
-        # y2_exp_unit[i] = np.inf
-        # y2_exp_dirc[i] = np.inf
-        # y2_exp_exp[i] = np.inf
 
 # Transfer to list
 x = x.tolist()
 h = h.tolist()
-hinv = hinv.tolist()
 y = y.tolist()
 y2 = y2.tolist()
-# # x list
-# x_unit = x_unit.tolist()
-# x_dirc = x_dirc.tolist()
-# x_exp = x_exp.tolist()
-# # h list
-# h_unit = h_unit.tolist()
-# h_dirc = h_dirc.tolist()
-# h_exp = h_exp.tolist()
-# # hinv list
-# hinv_unit = hinv_unit.tolist()
-# hinv_dirc = hinv_dirc.tolist()
-# hinv_exp = hinv_exp.tolist()
-# # y list
-# y_unit_unit = y_unit_unit.tolist()
-# y_unit_dirc = y_unit_dirc.tolist()
-# y_unit_exp = y_unit_exp.tolist()
-# y_dirc_unit = y_dirc_unit.tolist()
-# y_dirc_dirc = y_dirc_dirc.tolist()
-# y_dirc_exp = y_dirc_exp.tolist()
-# y_exp_unit = y_exp_unit.tolist()
-# y_exp_dirc = y_exp_dirc.tolist()
-# y_exp_exp = y_exp_exp.tolist()
-# # y2 list
-# y2_unit_unit = y2_unit_unit.tolist()
-# y2_unit_dirc = y2_unit_dirc.tolist()
-# y2_unit_exp = y2_unit_exp.tolist()
-# y2_dirc_unit = y2_dirc_unit.tolist()
-# y2_dirc_dirc = y2_dirc_dirc.tolist()
-# y2_dirc_exp = y2_dirc_exp.tolist()
-# y2_exp_unit = y2_exp_unit.tolist()
-# y2_exp_dirc = y2_exp_dirc.tolist()
-# y2_exp_exp = y_2exp_exp.tolist()
+# x list
+x_rect = x_rect.tolist()
+x_tri = x_tri.tolist()
+x_exp = x_exp.tolist()
+# h list
+h_rect = h_rect.tolist()
+h_tri = h_tri.tolist()
+h_exp = h_exp.tolist()
+# y list
+y_rect_rect = y_rect_rect.tolist()
+y_rect_tri = y_rect_tri.tolist()
+y_rect_exp = y_rect_exp.tolist()
+y_tri_rect = y_tri_rect.tolist()
+y_tri_tri = y_tri_tri.tolist()
+y_tri_exp = y_tri_exp.tolist()
+y_exp_rect = y_exp_rect.tolist()
+y_exp_tri = y_exp_tri.tolist()
+y_exp_exp = y_exp_exp.tolist()
 
 # Set up Bokeh data sources for the signals
 x_source = ColumnDataSource(data=dict(x=t, y1=x, y2=h))
-h_source = ColumnDataSource(data=dict(x=t, y1=x, y2=hinv))
+h_source = ColumnDataSource(data=dict(x=t, y1=x, y2=hshift))
 y_source = ColumnDataSource(data=dict(x=t, y=y))
 y2_source = ColumnDataSource(data=dict(x=t, y=y2))
+xfunc_source = ColumnDataSource(data=dict(x_rect=x_rect, x_tri=x_tri, x_exp=x_exp))        
+hfunc_source = ColumnDataSource(data=dict(h_rect=h_rect, h_tri=h_tri, h_exp=h_exp))  
+corr_source = ColumnDataSource(data=dict(y_rect_rect = y_rect_rect, 
+                                         y_rect_tri = y_rect_tri, 
+                                         y_rect_exp = y_rect_exp, 
+                                         y_tri_rect = y_tri_rect,
+                                         y_tri_tri = y_tri_tri,
+                                         y_tri_exp = y_tri_exp,
+                                         y_exp_rect = y_exp_rect,
+                                         y_exp_tri = y_exp_tri,
+                                         y_exp_exp = y_exp_exp))
+                                 
+
 # Set up Bokeh figures for the signals
-x_fig = figure(title='Input Signal', width=350, height=300, x_range=(-1, 4.01), y_range=(-1, 2))
+x_fig = figure(title='Input Signal', width=350, height=300, x_range=(-4.01, 4.01), y_range=(-1, 4))
 # x_fig.varea(x='x', y1='y1', y2='y2', source=x_source, fill_alpha=0.5)
 x_fig.line('x', 'y1', source=x_source, line_width=2, line_color='blue', legend_label="x(t)")
 x_fig.line('x', 'y2', source=x_source, line_width=2, line_color='red', line_dash="dotted", legend_label="h(t)")
 x_fig.legend.location = "top_left"
-h_fig = figure(title='Shift', width=350, height=300, x_range=(-1, 4.01), y_range=(-1, 2))
+h_fig = figure(title='Signal Shift', width=350, height=300, x_range=(-4.01, 4.01), y_range=(-1, 4))
 h_fig.line('x', 'y1', source=h_source, line_width=2, line_color='blue', legend_label="x(t-t')")
 h_fig.line('x', 'y2', source=h_source, line_width=2, line_color='red', line_dash="dotted", legend_label="h(t-t')")
+h_fig.varea(x='x', y1='y1', y2='y2', source=h_source, fill_alpha=0.2)
 h_fig.legend.location = "top_left"
-y_fig = figure(title='Output Signal', width=350, height=300, x_range=(-1, 4.01), y_range=(-1, 2))
+y_fig = figure(title='Output Signal', width=350, height=300, x_range=(-4.01, 4.01), y_range=(-1, 4))
 y_fig.line('x', 'y', source=y_source, line_width=2, line_color='green', line_alpha=0.2)
-y_fig.line('x', 'y', source=y2_source, line_width=2, line_color='green', legend_label="x(t)*h(t)")
+y_fig.line('x', 'y', source=y2_source, line_width=2, line_color='green', legend_label="correlation")
 y_fig.legend.location = "top_left"
 
 # Define time shift slider
@@ -154,47 +133,102 @@ shift_slider = Slider(title='t', value=-1, start=-1, end=4, step=0.006, width=34
 # shift_slider.on_change('value', update_data)
 
 # # Define the selector widget and its options
-# options = ['Unit Step', 'Dirac Impulse', 'Exponential']
-# x_selector = Select(title='Function:', value=options[0], options=options)
-# h_selector = Select(title='Function:', value=options[2], options=options)
-
-
-def update_data(attrname, old, new):
-    # Get the current value of the slider: current time
-    a = shift_slider.value
-    pts = int((a+1)/(t[1]-t[0]))
-    # Update the values of the data source: y2_source
-    hinv_new = flip(h, t, a)
-    h_source.data = dict(x=t, y1=x, y2=hinv_new)
-
-    # Update the values of the data source: y2_source
-    x2_new = t[:pts]
-    y2_new = y[:pts]
-    y2_source.data = dict(x=x2_new, y=y2_new)
+options = ['Rectangular', 'Triangular', 'Exponential']
+x_selector = Select(title='Signal 1:', value=options[0], options=options)
+h_selector = Select(title='Signal 2:', value=options[2], options=options)
 
 callback = CustomJS(args=dict(x_source=x_source, 
                               h_source=h_source, 
                               y_source=y_source, 
-                              y2_source=y2_source, 
-                              shiftval=shift_slider),
+                              y2_source=y2_source,
+                              xfunc_source=xfunc_source,
+                              hfunc_source=hfunc_source,
+                              corr_source=corr_source,
+                              shiftval=shift_slider,
+                              x_selector=x_selector,
+                              h_selector=h_selector),
                     code="""
     const xdata = x_source.data;
     const hdata = h_source.data;
     const ydata = y_source.data;
     const y2data = y2_source.data;
+    const xfuncdata = xfunc_source.data;
+    const hfuncdata = hfunc_source.data;
+    const corrdata = corr_source.data;
     const timeshift = shiftval.value;
-    // const x_func = x_selector.value;
-    // const h_func = h_selector.value;
+    const x_func = x_selector.value;
+    const h_func = h_selector.value;
     const t = xdata["x"];
     const x = xdata["y1"];
     const h = xdata["y2"];
-    const hinv = hdata["y2"];
+    const x2 = hdata["y1"]
+    const hshift = hdata["y2"];
     const y = ydata["y"];
     const t2 = y2data["x"];
     const y2 = y2data["y"];
+    let corry = corrdata["y_rect_exp"];
+    console.log(corry)
 
-
+    if (x_func == "Rectangular"){
+        if (h_func == "Rectangular"){
+            corry = corrdata["y_rect_rect"];
+        } else if (h_func == "Triangular"){
+            corry = corrdata["y_rect_dirc"];
+        }else if (h_func == "Exponential"){
+            corry = corrdata["y_rect_exp"];
+        }
+    } else if (x_func == "Triangular"){
+        if (h_func == "Rectangular"){
+            corry = corrdata["y_tri_rect"];
+        } else if (h_func == "Triangular"){
+            corry = corrdata["y_tri_tri"];
+        }else if (h_func == "Exponential"){
+            corry = corrdata["y_tri_exp"];
+        }
+    } else if (x_func == "Exponential"){
+        if (h_func == "Rectangular"){
+            corry = corrdata["y_exp_rect"];
+        } else if (h_func == "Triangular"){
+            corry = corrdata["y_exp_tri"];
+        }else if (h_func == "Exponential"){
+            corry = corrdata["y_exp_exp"];
+        }
+    }
+    
     const dt = t[1]-t[0];
+
+    if (x_func == "Rectangular"){
+        for (let i = 0; i < t.length ; i++) {
+            x[i] = xfuncdata["x_rect"][i];
+            x2[i] = xfuncdata["x_rect"][i];
+        }
+    } else if (x_func == "Triangular"){
+        for (let i = 0; i < t.length ; i++) {
+            x[i] = xfuncdata["x_tri"][i];
+            x2[i] = xfuncdata["x_tri"][i];
+        }
+    } else if (x_func == "Exponential"){
+        for (let i = 0; i < t.length ; i++) {
+            x[i] = xfuncdata["x_exp"][i];
+            x2[i] = xfuncdata["x_exp"][i];
+        }
+    }
+
+    if (h_func == "Rectangular"){
+        for (let i = 0; i < t.length ; i++) {
+            h[i] = hfuncdata["h_rect"][i];
+        }
+    } else if (h_func == "Triangular"){
+        for (let i = 0; i < t.length ; i++) {
+            h[i] = hfuncdata["h_tri"][i];
+        }
+    } else if (h_func == "Exponential"){
+        for (let i = 0; i < t.length ; i++) {
+            h[i] = hfuncdata["h_exp"][i];
+        }
+    }
+
+
     
     const shift = Math.round((timeshift+2)/dt);
     const result = h.slice(0, shift + 1).reverse(); 
@@ -204,7 +238,11 @@ callback = CustomJS(args=dict(x_source=x_source,
     }
 
     for (let i = 0; i < result.length ; i++) {
-        hinv[i] = result[i];
+        hshift[i] = result[i];
+    }
+
+    for (let i = 0; i < corry.length ; i++) {
+        y[i] = corry[i];
     }
 
     const pts = Math.round((timeshift+1)/dt);
@@ -214,18 +252,20 @@ callback = CustomJS(args=dict(x_source=x_source,
     for (let i = pts; i < result.length ; i++) {
         y2[i] = Infinity;
     }
-    console.log(y2);
 
+    x_source.change.emit();
     h_source.change.emit();
+    y_source.change.emit();
     y2_source.change.emit();
 """)
 
 shift_slider.js_on_change('value', callback)
-
+x_selector.js_on_change('value', callback)
+h_selector.js_on_change('value', callback)
 # Combine the figures and sliders into a Bokeh layout
-# layout = gridplot([[row(x_fig, h_fig)], 
-#                    [row(column(x_selector, h_selector, shift_slider), y_fig)]])
-layout = gridplot([[row(x_fig, h_fig)], [row(shift_slider, y_fig)]])
+layout = gridplot([[row(x_fig, h_fig)], 
+                   [row(column(x_selector, h_selector, shift_slider), y_fig)]])
+# layout = gridplot([[row(x_fig, h_fig)], [row(shift_slider, y_fig)]])
 
 show(layout)
 # bokeh serve --show --port 5001 convolution.py
