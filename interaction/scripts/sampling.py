cee095044c8b2e4c817f1e889ff5e258f9489eae
Jiaying Li <jiaying.li0803@gmail.com>
2023-03-25 16:22:51 -0400

    sampling interaction

--- interaction/scripts/sampling.py
+++ interaction/scripts/sampling.py
@@ -1,13 +1,48 @@
-from collections import OrderedDict
-from bokeh.charts import Dot, output_file, show
+from bokeh.layouts import column, row, gridplot
+from bokeh.models import ColumnDataSource, CustomJS, Slider, Select
+from bokeh.plotting import figure, output_file, show, curdoc, save
+import numpy as np
 
 # dict, OrderedDict, lists, arrays and DataFrames are valid inputs
-xyvalues = OrderedDict()
-xyvalues['python']=[2, 5]
-xyvalues['pypy']=[12, 40]
-xyvalues['jython']=[22, 30]
 
-dot = Dot(xyvalues, ['cpu1', 'cpu2'], title='dots')
 
-output_file('dot.html')
-show(dot)
+t1 = np.linspace(-5, 5, 50)
+y1 = np.sin(t1)
+origin = y1.tolist()
+
+t2 = t1[::4]
+y2 = np.sin(t2)
+sample = origin[::4]
+
+origin_source = ColumnDataSource(data=dict(x=t1, y=origin))
+sample_source = ColumnDataSource(data=dict(x=t2, y=sample))
+
+x_fig = figure(title='Origin Signal', width=350, height=300, x_range=(-5, 5), y_range=(-3, 3))
+x_fig.scatter('x', 'y', source=origin_source, size=3, color="blue", legend_label="origin", fill_alpha=0.6, line_alpha=0.6)
+
+y_fig = figure(title='Sample Signal', width=350, height=300, x_range=(-5, 5), y_range=(-3, 3))
+y_fig.scatter('x', 'y', source=origin_source, size=3, color="blue", legend_label="origin", fill_alpha=0.2, line_alpha=0.2)
+y_fig.scatter('x', 'y', source=sample_source, size=3, color='blue', legend_label="sample", fill_alpha=0.8, line_alpha=0.8)
+
+slider = Slider(title='step', value=1, start=1, end=10, step=1, width=700, height=300)
+
+callback = CustomJS(args=dict(source=source, amp=amp_slider, freq=freq_slider, phase=phase_slider, offset=offset_slider),
+                    code="""
+    const data = source.data;
+    const A = amp.value;
+    const k = freq.value;
+    const phi = phase.value;
+    const B = offset.value;
+    const x = data['x']
+    const y = data['y']
+    for (let i = 0; i < x.length; i++) {
+        y[i] = B + A*Math.sin(k*x[i]+phi);
+    }
+    source.change.emit();
+""")
+
+amp_slider.js_on_change('value', callback)
+
+
+layout=column(row(x_fig, y_fig), slider)
+show(layout)
